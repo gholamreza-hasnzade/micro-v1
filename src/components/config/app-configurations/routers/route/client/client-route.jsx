7d31f6469b5a0adf5c2225e349479b1c2f009b20
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-12 17:12:54 +0330

    fixed route

--- src/components/config/app-configurations/routers/route/client/client-route.jsx
+++ src/components/config/app-configurations/routers/route/client/client-route.jsx
@@ -1,6 +1,5 @@
-/* import { ClientView } from "@components/views";
- */
 import { ClientView } from "@components/views/client-view/client-view";
+
 const ClientRoute = [
     {
         path: "/client",
--- src/components/config/app-configurations/routers/route/home/home-route.jsx
+++ src/components/config/app-configurations/routers/route/home/home-route.jsx
@@ -1,11 +1,14 @@
 import { HomeView } from "@components/views/home-view/home-view";
-
-/* import { HomeView } from "@components/views";
- */
+import { Navigate } from "react-router-dom";
 
 const HomeRoute = [
     {
         path: "/",
+        element: <Navigate to={"/home"} />,
+        exact: true,
+    },
+    {
+        path: "/home",
         element: <HomeView />,
         exact: true,
     },
