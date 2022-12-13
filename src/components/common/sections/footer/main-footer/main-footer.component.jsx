ec135b806d018c35eda787adfed6eb86a0e47dd8
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-13 14:06:58 +0330

    create footer

--- src/components/common/sections/footer/main-footer/main-footer.component.jsx
+++ src/components/common/sections/footer/main-footer/main-footer.component.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+// * import tools
+import React from "react";
+
+// * import style
+import { MainFooterStyle as S } from "@components/common/sections/footer/main-footer/main-footer.component.style";
 
 export const MainFooterComponent = () => {
-  return (
-    <div>MainFooterComponent</div>
-  )
-}
+    return (
+        <S.Footer component={"footer"}>
+            <S.FooterTitle>© ۱۴۰۱ - تمامی حقوق برای شرکت مهندسی نرم افزاری هلو محفوظ است.</S.FooterTitle>
+        </S.Footer>
+    );
+};
---
+++ src/components/common/sections/footer/main-footer/main-footer.component.style.jsx
@@ -0,0 +1,25 @@
+// * Import Tools
+import styled from "@emotion/styled/macro";
+import { Box, Typography } from "@mui/material";
+
+const Footer = styled(Box)`
+    width: 100%;
+    padding: 25px;
+    border-radius: 6px;
+    background: #404258;
+    display: flex;
+  
+`;
+
+const FooterTitle = styled(Typography)`
+    text-align: center;
+    color: ${({ theme }) => theme.PRIMARY};
+    font-size: 12px;
+    aligin-item: center;
+    width: 100%;
+`;
+
+export const MainFooterStyle = {
+    Footer,
+    FooterTitle,
+};
