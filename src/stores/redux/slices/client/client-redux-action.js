a3430b126d3ab7a4c1834f4ce0834393832402ad
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-20 13:39:17 +0330

    fixed bug pagentions

--- src/stores/redux/slices/client/client-redux-action.js
+++ src/stores/redux/slices/client/client-redux-action.js
@@ -8,12 +8,15 @@ import {
 // * import redux
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const getUsers = createAsyncThunk("getUsers", async () => {
+export const getUsers = createAsyncThunk("getUsers", async (page) => {
     const { baseURL9000, v1 } = endpoints;
     const response = await callApi({
         baseURL: baseURL9000,
         url: `${v1}/user`,
         method: requestMethodes.get,
+        params: {
+            page: page,
+        },
     });
     if (response?.status) {
         return {
