5cd7239834da6c1ffaebc07904acd43449d1a68b
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-20 14:37:28 +0330

    change style in skeleton

--- src/components/common/partials/loadings/skeleton/skeleton-component.style.jsx
+++ src/components/common/partials/loadings/skeleton/skeleton-component.style.jsx
@@ -3,10 +3,9 @@ import styled from "@emotion/styled/macro";
 import { Skeleton as SkeletonMUI } from "@mui/material";
 
 const Skeleton = styled(SkeletonMUI)`
-
-
-`
+    background-color: rgb(205 178 178 / 11%);
+`;
 
 export const SkeletonComponentStyle = {
-    Skeleton
+    Skeleton,
 };
