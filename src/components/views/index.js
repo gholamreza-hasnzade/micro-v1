9cad01116d5073dc543187701db8848abf450ecd
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-25 17:50:37 +0330

    delete code

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,25 +1,6 @@
 import { lazy } from "react";
 
 // * import Client View
-/* export { ClientView } from "@components/views/client-view/client-view";
-export { ClientAddView } from "@components/views/client-view/add/client-add-view";
-export { ClientEditView } from "@components/views/client-view/edit/client-edit-view";
-export { PreviewClienView } from "@components/views/client-view/preview/preview-client-view"; */
-
-/* const { ClientView } = lazy(() =>
-    import("@components/views/client-view/client-view")
-);
-const { ClientAddView } = lazy(() =>
-    import("@components/views/client-view/add/client-add-view")
-);
-const ClientEditView = lazy(() =>
-    import("@components/views/client-view/edit/client-edit-view")
-);
-const { PreviewClienView } = lazy(() =>
-    import("@components/views/client-view/preview/preview-client-view")
-);
-
-export { ClientView, ClientAddView, ClientEditView, PreviewClienView }; */
 const ClientView = lazy(() =>
     import("@components/views/client-view/client-view")
 );
