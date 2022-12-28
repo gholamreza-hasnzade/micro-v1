c2b7ae2a1fb8bef4761f7c8c3a1199dc604ec1c0
gh.hassanzadeh <gholamreza15402@gmail.com>
2022-12-28 11:09:20 +0330

    loading page in routes

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,6 @@ import AppRouter from "@components/config/app-configurations/routers/router";
 
 import { AppConfigurations } from "./config/app-configurations/app.configurations";
 
-// * Import components
-import { Loader } from "@components/common/partials";
-
 // ** React Toastify css
 import "react-toastify/dist/ReactToastify.css";
 
@@ -18,12 +15,10 @@ const App = () => {
     const { t } = useTranslation();
     document.title = t("online shoping holoo");
     return (
-        <Suspense fallback={<Loader />}>
-            <AppConfigurations>
-                <AppRouter />
-                <ToastContainer />
-            </AppConfigurations>
-        </Suspense>
+        <AppConfigurations>
+            <AppRouter />
+            <ToastContainer />
+        </AppConfigurations>
     );
 };
 
--- src/components/config/app-configurations/routers/router.js
+++ src/components/config/app-configurations/routers/router.js
@@ -1,5 +1,5 @@
 // * import tools
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 
 // * import components/routes
@@ -7,31 +7,48 @@ import { RoutesPage } from "@components/config/app-configurations/routers/route/
 
 // * import components/layout
 import { MainLayout } from "@components/common/sections";
+
+// * Import components
+import { Loader } from "@components/common/partials";
+
 const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {RoutesPage?.map((route, idx) => {
-                    return (
-                        route.element && (
-                            <Route
-                                key={idx}
-                                path={route.path}
-                                exact={route.exact}
-                                name={route.name}
-                                element={
-                                    <MainLayout>
-                                        <route.element />
-                                    </MainLayout>
-                                }
-                            />
-                        )
-                    );
-                })}
-              {/*   <Route path="/" element={<Navigate to="home" replace />} /> */}
-            </Routes>
-        </BrowserRouter>
-    );
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        if (loading) {
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+        }
+    }, [loading]);
+
+    if (loading) {
+        return <Loader />;
+    } else {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    {RoutesPage?.map((route, idx) => {
+                        return (
+                            route.element && (
+                                <Route
+                                    key={idx}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    name={route.name}
+                                    element={
+                                        <MainLayout>
+                                            <route.element />
+                                        </MainLayout>
+                                    }
+                                />
+                            )
+                        );
+                    })}
+                    {/*   <Route path="/" element={<Navigate to="home" replace />} /> */}
+                </Routes>
+            </BrowserRouter>
+        );
+    }
 };
 
 export default AppRouter;
