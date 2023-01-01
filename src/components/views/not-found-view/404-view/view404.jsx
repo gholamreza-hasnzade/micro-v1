8c79d4b36acb432171157f2a403ccf5528eade41
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-01-01 12:05:22 +0330

    fixed bug title

--- src/components/views/not-found-view/404-view/view404.jsx
+++ src/components/views/not-found-view/404-view/view404.jsx
@@ -1,11 +1,11 @@
 // * Import tools
 import React from "react";
 import { useTranslation } from "react-i18next";
-
 // * Import Style
 import { View404Style as S } from "@components/views/not-found-view/404-view/view404.style";
-// * import constant
+// * Import constant
 import { IMAGES } from "@constants/content";
+// * Import components
 import { LinkButton } from "@components/common/partials";
 
 export const View404 = () => {
@@ -23,7 +23,7 @@ export const View404 = () => {
                 </S.Wapper>
             </S.Row>
             <S.Typography>{t("not found page 404")}</S.Typography>
-            <LinkButton href="/">صفحه اصلی</LinkButton>
+            <LinkButton href="/">{t("main page")} </LinkButton>
         </S.Container>
     );
 };
